21b0f604ebf2d87b7e93d750649f541fa74f86e8
ruush <ruushekas@gmail.com>
2026-07-09 20:01:56 +0530

    GFG DAY-22

---
+++ GFG/LinkedList/rotateLL.py
@@ -0,0 +1,43 @@
+""" Node Structure
+class Node:
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+"""
+
+class Solution:
+    def reorderList(self, head):
+        # code here
+        if not head or not head.next:
+            return
+        
+        slow=head
+        fast=head
+        
+        while fast.next and fast.next.next:
+            slow=slow.next
+            fast=fast.next.next
+            
+        prev=None
+        curr=slow.next
+        slow.next=None
+        
+        while curr:
+            nxt=curr.next
+            curr.next=prev
+            prev=curr
+            curr=nxt
+            
+        first=head
+        second=prev
+        
+        while second:
+            t1=first.next
+            t2=second.next
+            
+            first.next=second
+            second.next=t1
+            
+            first=t1
+            second=t2
+            
