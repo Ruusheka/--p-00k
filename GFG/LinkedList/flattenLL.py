283842ac1325aadbc3594a175c6d90d26c3bdeb2
ruush <ruushekas@gmail.com>
2026-07-13 21:37:58 +0530

    GFG DAY-24

---
+++ GFG/LinkedList/flattenLL.py
@@ -0,0 +1,34 @@
+'''
+class Node:
+    def __init__(self, d):
+        self.data=d
+        self.next=None
+        self.bottom=None
+        
+'''
+
+class Solution:
+    def merge(self,a,b):
+        if not a:
+            return b
+        if not b:
+            return a
+            
+        if a.data<b.data:
+            result=a
+            result.bottom = self.merge(a.bottom,b)
+        else:
+            result=b
+            result.bottom=self.merge(a,b.bottom)
+            
+        result.next = None
+        return result
+        
+            
+    def flatten(self, root):
+        # code here
+        if not root or not root.next:
+            return root
+        
+        root.next = self.flatten(root.next)
+        return self.merge(root,root.next)
