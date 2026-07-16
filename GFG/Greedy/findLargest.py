e936bf0dc622d5d56c0a5a8c14cb08cb94f31f82
ruush <ruushekas@gmail.com>
2026-07-16 22:59:20 +0530

    GFG DAY-26

---
+++ GFG/Greedy/findLargest.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findLargest(self, n, s):
+        # code here
+        if s>9*n:
+            return '-1'
+        
+        if s==0:
+            if n==1:
+                return '0'
+            else:
+                return '-1'
+                
+        ans=[]
+        
+        for _ in range(n):
+            digit=min(9,s)
+            ans.append(str(digit))
+            s-=digit
+        
+        return ''.join(ans)
